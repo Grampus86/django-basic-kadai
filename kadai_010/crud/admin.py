f4f8385a79975d46d6bae5e667e794f795cfb188
Kota Takahashi <59428418+Grampus86@users.noreply.github.com>
2024-03-31 12:48:44 +0900

    kadai_012を追加

---
+++ kadai_010/crud/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from crud.models import Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price')
+    search_fields = ('name',)
+
+
+admin.site.register(Product, ProductAdmin)
+
+# Register your models here.
